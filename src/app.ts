2f2f4d7237a27ed1d463e7a0bdac4d2ae139a7a9
Abinash Sahoo <implabinash@gmail.com>
2025-10-16 11:50:34 +0530

    fix: remove unnessary code

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,10 @@
 import { Hono } from "hono";
 
-import userRoute from "@/routes/users.route";
-
 type Bindings = {
     DB: D1Database;
 };
 
 const app = new Hono<{ Bindings: Bindings }>().basePath("/api/v1");
-
-app.route("/users", userRoute);
 
 app.get("/health", (c) => {
     const result = {
--- src/routes/users.route.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-
-import { getAllUsers } from "@/controllers/users.controller";
-
-const userRoute = new Hono();
-
-userRoute.get("/", getAllUsers);
-
-export default userRoute;
