c5d9c0cef5bc7a6a750c497f4fc999cda9070c71
Abinash Sahoo <implabinash@gmail.com>
2025-10-20 09:35:49 +0530

    fix: simplify the hashing of password

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -6,15 +6,4 @@ const authRoute = new Hono();
 
 authRoute.post("/signup/email", signUpWithEmail);
 
-/*
-    1. /signup/google
-
-    2. /signin/email
-    3. /signin/google
-
-    4. /logout
-
-    5. /change-password
-*/
-
 export default authRoute;
--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,26 +1,9 @@
-import { scrypt } from "@noble/hashes/scrypt.js";
-import { randomBytes } from "@noble/hashes/utils.js";
+import bcrypt from "bcryptjs";
 
-export async function hashPassword(password: string): Promise<string> {
-    const salt = randomBytes(16);
-    const hash = scrypt(password, salt, { N: 2 ** 14, r: 8, p: 1, dkLen: 32 });
+export const hashPassword = async (password: string) => {
+    return bcrypt.hash(password, 10);
+};
 
-    const combined = new Uint8Array(salt.length + hash.length);
-    combined.set(salt);
-    combined.set(hash, salt.length);
-
-    return Buffer.from(combined).toString("base64");
-}
-
-export async function verifyPassword(
-    password: string,
-    stored: string,
-): Promise<boolean> {
-    const combined = Buffer.from(stored, "base64");
-    const salt = combined.slice(0, 16);
-    const originalHash = combined.slice(16);
-
-    const hash = scrypt(password, salt, { N: 2 ** 14, r: 8, p: 1, dkLen: 32 });
-
-    return Buffer.from(hash).equals(originalHash);
-}
+export const verifyPassword = async (password: string, storedHash: string) => {
+    return bcrypt.compare(password, storedHash);
+};
