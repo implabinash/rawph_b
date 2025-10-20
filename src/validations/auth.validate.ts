1faf89d72003f932f68ea85a74b9f3744f445cfa
Abinash Sahoo <implabinash@gmail.com>
2025-10-20 14:27:11 +0530

    fix: add refine to change password validation

--- src/validations/auth.validate.ts
+++ src/validations/auth.validate.ts
@@ -34,28 +34,33 @@ export const signInSchema = z.object({
         .min(6, { message: "Password must be at least 6 characters long" }),
 });
 
-export const changePasswordSchema = z.object({
-    currentPassword: z
-        .string()
-        .min(6, { message: "Password must be at least 6 characters long" }),
-    newPassword: z
-        .string()
-        .min(6, { message: "Password must be at least 6 characters long" })
-        .max(16, { message: "Password must not exceed 16 characters" })
-        .regex(/[0-9]/, {
-            message: "Password must contain at least one number",
-        })
-        .regex(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, {
-            message: "Password must contain at least one special character",
-        }),
-    confirmPassword: z
-        .string()
-        .min(6, { message: "Password must be at least 6 characters long" })
-        .max(16, { message: "Password must not exceed 16 characters" })
-        .regex(/[0-9]/, {
-            message: "Password must contain at least one number",
-        })
-        .regex(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, {
-            message: "Password must contain at least one special character",
-        }),
-});
+export const changePasswordSchema = z
+    .object({
+        currentPassword: z
+            .string()
+            .min(6, { message: "Password must be at least 6 characters long" }),
+        newPassword: z
+            .string()
+            .min(6, { message: "Password must be at least 6 characters long" })
+            .max(16, { message: "Password must not exceed 16 characters" })
+            .regex(/[0-9]/, {
+                message: "Password must contain at least one number",
+            })
+            .regex(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, {
+                message: "Password must contain at least one special character",
+            }),
+        confirmPassword: z
+            .string()
+            .min(6, { message: "Password must be at least 6 characters long" })
+            .max(16, { message: "Password must not exceed 16 characters" })
+            .regex(/[0-9]/, {
+                message: "Password must contain at least one number",
+            })
+            .regex(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, {
+                message: "Password must contain at least one special character",
+            }),
+    })
+    .refine((data) => data.newPassword === data.confirmPassword, {
+        message: "New password and confirm password must match",
+        path: ["confirmPassword"],
+    });
