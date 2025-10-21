55a5aadc874911477f995a37670ab14b5cc01e88
Abinash Sahoo <implabinash@gmail.com>
2025-10-21 14:43:43 +0530

    fix: local drizzle config

--- drizzle.config.local.ts
+++ drizzle.config.local.ts
@@ -5,6 +5,6 @@ export default defineConfig({
     schema: "./src/db/schemas",
     dialect: "sqlite",
     dbCredentials: {
-        url: ".wrangler/state/v3/d1/miniflare-D1DatabaseObject/37864b0e9832feac3413f1d3586feb786f11f03ed497e5afbe0c56e8934f967c.sqlite",
+        url: ".wrangler/state/v3/d1/miniflare-D1DatabaseObject/1c1df971e04a45ac4fbc5c9c7d87ff9d3c97ee5f15a71452c8a50439d4dc88ff.sqlite",
     },
 });
